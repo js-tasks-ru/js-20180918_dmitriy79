9d6a2e03c701b2c8bd858872af76c80c05a6feb8
Dmitrii Levchenko <dlxeon@gmail.com>
2018-09-28 16:19:06 +0300

    Intermediate result

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,22 @@
  * @returns {Object}
  */
 function clone (obj) {
+    if (obj === null) {
+        return null;
+    }
+    if (obj === undefined) {
+        return undefined;
+    }
+    let cloned = {};
+    for (let prop in obj) {
+        const propValue = obj[prop];
+        if (obj[prop] === null) {
+            cloned[prop] = null;
+        } else if (typeof propValue === 'object') {
+            cloned[prop] = clone(propValue);
+        } else {
+            cloned[prop] = propValue;
+        }
+    }
+    return cloned;
 }
